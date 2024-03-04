d6bdb39863092f9a67de9407bb1e4337945b56d1
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-03-04 13:22:55 +0900

    add: sample code

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AnovReader;
+
+// input test that is written in Anov Syntax.
+string input_1 = "- a girl";
+string input_2 = "[Hello, World!]";
+
+Anov.Read(input_1);
+Anov.Read(input_2);
