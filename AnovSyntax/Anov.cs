567eb62f56093fcfd021559abda2a0c0bcc49eaa
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-03-16 19:39:13 +0900

    change: to english style

--- AnovSyntax/Anov.cs
+++ AnovSyntax/Anov.cs
@@ -7,14 +7,15 @@ public class Anov
     public static void Read(string str)
     {
         Match match;
-        // "- "から始まる"人物"を読み込み
+
+        // Read "- people-name"
         match = Regex.Match(str, @"- (.*)");
         if (match.Success)
             Console.Write(match.Groups[1].Value);
 
-        // "["と"]"で囲む"会話"を読み込み
+        // Read "[conversatioc-content]"
         match = Regex.Match(str, @"\[(.*?)\]");
         if (match.Success)
-            Console.WriteLine("「" + match.Groups[1].Value + "」");
+            Console.WriteLine(" \"" + match.Groups[1].Value + "\"");
     }
 }
