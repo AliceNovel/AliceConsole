bc222f39334f7ad7bf5e09c8473a2998cc5fe9a6
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-04-09 21:56:02 +0900

    add: emotion support

--- AnovSyntax/Anov.cs
+++ AnovSyntax/Anov.cs
@@ -12,10 +12,22 @@ public class Anov
     {
         Match match;
 
-        // Read "- people-name"
-        match = Regex.Match(str, @"- (.*)");
+        // Read "- people-name / emotion"
+        match = Regex.Match(str, @"- (.*?)/");
         if (match.Success)
             Console.Write(match.Groups[1].Value.Trim());
+        else
+        {
+            // Read "- people-name"
+            match = Regex.Match(str, @"- (.*)");
+            if (match.Success)
+                Console.Write(match.Groups[1].Value.Trim());
+        }
+
+        // Read "/ emotion"
+        match = Regex.Match(str, @"/ (.*)");
+        if (match.Success)
+            Console.Write(" (" + match.Groups[1].Value.Trim() + ")");
 
         // Read "[conversatioc-content]"
         match = Regex.Match(str, @"\[(.*?)\]");
