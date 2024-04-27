fcb75f3a4a39acbc45dc71f2f1168ebb8ea29a62
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-04-27 17:19:11 +0900

    change: out put method (fix bug that was not working on `- people-name / emotion`)

--- AnovSyntax/Anov.cs
+++ AnovSyntax/Anov.cs
@@ -11,47 +11,48 @@ public class Anov
     public static string Read(string str)
     {
         Match match;
+        string _return = "";
 
         // Unsupported
         // Read "> place"
         match = Regex.Match(str, @"> (.*)");
         if (match.Success)
-            return("<place>");
+            _return += "<place>";
 
         // Unsupported
         // Read "bgm: background-music"
         match = Regex.Match(str, @"bgm: (.*)");
         if (match.Success)
-            return("<bgm>");
+            _return += "<bgm>";
 
         // Unsupported
         // Read "movie: movie"
         match = Regex.Match(str, @"movie: (.*)");
         if (match.Success)
-            return("<movie>");
+            _return += "<movie>";
 
         // Read "- people-name / emotion"
         match = Regex.Match(str, @"- (.*?)/");
         if (match.Success)
-            return(match.Groups[1].Value.Trim());
+            _return += match.Groups[1].Value.Trim();
         else
         {
             // Read "- people-name"
             match = Regex.Match(str, @"- (.*)");
             if (match.Success)
-                return(match.Groups[1].Value.Trim());
+                _return += match.Groups[1].Value.Trim();
         }
 
         // Read "/ emotion"
         match = Regex.Match(str, @"/ (.*)");
         if (match.Success)
-            return(" (" + match.Groups[1].Value.Trim() + ")");
+            _return += " (" + match.Groups[1].Value.Trim() + ")";
 
         // Read "[conversatioc-content]"
         match = Regex.Match(str, @"\[(.*?)\]");
         if (match.Success)
-            return(" \"" + match.Groups[1].Value.Trim() + "\"");
+            _return += " \"" + match.Groups[1].Value.Trim() + "\"";
 
-        return("");
+        return _return;
     }
 }
