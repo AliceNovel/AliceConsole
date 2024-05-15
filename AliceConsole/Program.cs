50f72a758f0504fde677a3a2e3c692b97062ad6a
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-05-15 17:36:37 +0900

    support: file input

--- AliceConsole/Program.cs
+++ AliceConsole/Program.cs
@@ -1,17 +1,37 @@
 ﻿using AnovSyntax;
 
-// input test that is written in Anov Syntax.
-string filePath = @"sample.anov";
-
-using (StreamReader sr = new(filePath))
+class Program
 {
-    while (!sr.EndOfStream)
+    static void Main(string[] args)
     {
-        string? line = sr.ReadLine();
-        if (line == "")
+        if (args.Length == 0 || args[0] == "-h" || args[0] == "--help")
+        {
+            Console.WriteLine("Usage: ./AliceConsole <path>");
+            Console.WriteLine("Example: ./AliceConsole sample.anov");
+            return;
+        }
+
+        // input test that is written in Anov Syntax.
+        string filePath = args[0];
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("Error: File does not exist.");
+            Console.WriteLine("Hint: Please check that the name is correct.");
+            return;
+        }
+
+        using (StreamReader sr = new(filePath))
+        {
+            while (!sr.EndOfStream)
+            {
+                string? line = sr.ReadLine();
+                if (line == "")
+                    Console.ReadLine();
+                else if (line is not null)
+                    Console.WriteLine(Anov.Read(line));
+            }
             Console.ReadLine();
-        else if (line is not null)
-            Console.WriteLine(Anov.Read(line));
+        }
     }
-    Console.ReadLine();
 }
