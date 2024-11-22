1af27edebbc14f4eb9502325085d18cd9f832c2c
Lemon73-Computing <Lemonade.flash.1164@gmail.com>
2024-11-23 01:29:38 +0900

    feat(init): add init command

--- AliceConsole/Program.cs
+++ AliceConsole/Program.cs
@@ -4,11 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length == 0 || args[0] == "-h" || args[0] == "--help")
+        if (args.Length == 0)
         {
+            Console.WriteLine("Alice Console");
+            return;
+        }
+
+        if (args[0] == "-h" || args[0] == "--help")
+        {
+            Console.WriteLine("# Read Anov Syntax #");
             Console.WriteLine("Usage: ./AliceConsole <path>");
             Console.WriteLine("Example: ./AliceConsole sample.anov");
+            Console.WriteLine();
+            Console.WriteLine("# Init anproj file #");
+            Console.WriteLine("Usage: ./AliceConsole init");
             return;
+        }
+
+        if  (args[0] == "init")
+        {
+            string outputDirectoryName = @"AnprojTemplate";
+
+            try
+            {
+                if (Directory.Exists(outputDirectoryName))
+                {
+                    Console.WriteLine("The path exists already. Please remove \"AnprojTemplate\" directory.");
+                    return;
+                }
+                Directory.CreateDirectory(outputDirectoryName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
 
         // input test that is written in Anov Syntax.
